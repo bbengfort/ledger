2523efacecec08627393e7934eaa1304cdbc01f3
Benjamin Bengfort <benjamin@bengfort.com>
2022-11-23 07:38:45 -0600

    version bump v1.4.3

--- ledger/version.py
+++ ledger/version.py
@@ -27,9 +27,9 @@ SLUG_COMMIT_ENV = [
 __version_info__ = {
     'major': 1,
     'minor': 4,
-    'micro': 2,
+    'micro': 3,
     'releaselevel': 'final',
-    'serial': 64,
+    'serial': 65,
 }
 
 
